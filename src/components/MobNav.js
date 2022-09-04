36118a1791e027b887e0746de257d43869c263c9
DizzlyD <david4henery00@hotmail.com>
2022-09-04 14:43:46 +0100

    layout - responsive

--- src/components/MobNav.js
+++ src/components/MobNav.js
@@ -3,11 +3,25 @@ import {Hamburger, Logo} from "../svgs/index";
 
 const MobNav = () => {
   return (
-    <nav className='nav'>
-      <Hamburger className='nav__burger'/>
-      <Logo className='nav__logo'/>
+    <nav className="nav">
+      <Hamburger className="nav__burger" />
+      <Logo className="nav__logo" />
+      <ul className="nav-links">
+        <li className="nav-link">
+          <button className="nav-link__btn">Home</button>
+        </li>
+        <li className="nav-link">
+          <button className="nav-link__btn">Shop</button>
+        </li>
+        <li className="nav-link">
+          <button className="nav-link__btn">About</button>
+        </li>
+        <li className="nav-link">
+          <button className="nav-link__btn">Contact</button>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default MobNav
