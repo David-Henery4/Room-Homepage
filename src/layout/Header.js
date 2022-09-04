69c96b1b77cf8f96f9e17580927df29ba80d6189
DizzlyD <david4henery00@hotmail.com>
2022-09-04 20:55:33 +0100

    whole-layout

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MobNav } from "../components";
 import {IconLeft, IconRight, Arrow} from "../svgs";
-import heroImg from "../images/mobile-image-hero-1.jpg";
-import heroImgDesk from "../images/desktop-image-hero-1.jpg";
+import {textAndImages } from "../image-arrays/images";
 
 
 const Header = () => {
+  const [slider, setSlider] = useState(0)
+  //
+  const incrementSider = () => {
+    setSlider(slider + 1)
+  }
+  //
+  const deincrementSider = () => {
+    setSlider(slider - 1)
+  }
+  //
+  useEffect(() => {
+    if (slider > textAndImages.length - 1) {
+      setSlider( (i) => i = 0);
+    }
+    if (slider < 0) {
+      setSlider((i) => i = textAndImages.length - 1);
+    }
+  }, [slider])
+  //
   return (
     <main className="header header-grid">
       <MobNav />
       <section className="header-img">
         <picture>
-          <source srcSet={heroImgDesk} media="(min-width: 28.12em)"/>
-        <img className="header-hero-img" src={heroImg} alt="hero-img"/>
+          <source
+            srcSet={textAndImages[slider] && textAndImages[slider].deskImg}
+            media="(min-width: 28.12em)"
+          />
+          <img
+            className="header-hero-img"
+            src={textAndImages[slider] && textAndImages[slider].mobImg}
+            alt="hero-img"
+          />
         </picture>
       </section>
       <div className="header-slider-nav">
-        <IconLeft className="header-slider__icon" />
-        <IconRight className="header-slider__icon" />
+        <IconLeft className="header-slider__icon" onClick={deincrementSider} />
+        <IconRight className="header-slider__icon" onClick={incrementSider} />
       </div>
       <section className="header-info">
-        <h1 className="header__title">Discover innovative ways to decorate</h1>
+        <h1 className="header__title">
+          {textAndImages[slider] && textAndImages[slider].title}
+        </h1>
         <p className="header__text">
-          We provide unmatched quality, comfort, and style for property owners
-          across the country. Our experts combine form and function in bringing
-          your vision to life. Create a room in your own style with our
-          collection and make your property a reflection of you and what you
-          love.
+          {textAndImages[slider] && textAndImages[slider].text}
         </p>
         <div className="header-cta">
           <p className="header-cta__btn">SHOP NOW</p>
